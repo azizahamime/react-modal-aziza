fb898e8876e1553be111728693efb83a059ea942
Aziza H <mess.aziza@gmail.com>
2023-06-23 08:51:14 +0200

    style btn close et modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,22 +2,46 @@ import React, { Fragment, useState } from 'react'
 import styles from './modal.css'
 
 export const ModalContent = (props) => {
-  const { crossClose, message, btnClose } = props
+  const { crossClose, message, btnClose, btnStyle, modalStyle } = props
+  const { btnBackgroundColor, btnTextColor } = btnStyle
+  const { modalBackground, modalTextColor } = modalStyle
+
   const [isOpenModal, setIsOpenModal] = useState(true)
   const toggleModal = () => setIsOpenModal(!isOpenModal)
+
   return (
     <Fragment>
       {isOpenModal && (
         <div className={styles.container}>
-          <div className={styles.modal}>
+          <div
+            className={styles.modal}
+            style={{
+              backgroundColor: modalBackground || 'black',
+              color: modalTextColor || 'white'
+            }}
+          >
             {crossClose && (
-              <button className={styles.cross} onClick={toggleModal}>
+              <button
+                className={styles.cross}
+                onClick={toggleModal}
+                style={{
+                  backgroundColor: btnBackgroundColor || 'black',
+                  color: btnTextColor || 'white'
+                }}
+              >
                 x
               </button>
             )}
             <p>{message || 'Enregistrer !!'}</p>
             {btnClose && (
-              <button className={styles.close} onClick={toggleModal}>
+              <button
+                className={styles.close}
+                onClick={toggleModal}
+                style={{
+                  backgroundColor: btnBackgroundColor || 'black',
+                  color: btnTextColor || 'white'
+                }}
+              >
                 close
               </button>
             )}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import { ModalContent } from './components/modal/modal.jsx'
 export const Modal = () => {
   return (
     <Fragment>
-      <ModalContent message='helloo' btnClose crossClose />
+      <ModalContent
+        message='helloo'
+        btnClose
+        crossClose
+      // eslint-disable-next-line prettier/prettier
+      /* btnStyle={{ btnBackgroundColor: '#f2f2f2', btnTextColor: '#000' }}
+    modalStyle={{ modalBackground: '#000', color: '#fff' }} */
+      />
     </Fragment>
   )
 }
