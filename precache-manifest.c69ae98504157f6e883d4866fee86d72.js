d83a92a44b773f18b4c18e80250f7ae90fbcbe8a
Aziza H <mess.aziza@gmail.com>
2023-07-02 10:27:37 +0200

    Updates

---
+++ precache-manifest.c69ae98504157f6e883d4866fee86d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e96a6386491911f95e4597a1b9ecc0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3aecf9506830afa939e9",
+    "url": "./static/css/main.1dca62e3.chunk.css"
+  },
+  {
+    "revision": "539eeed9588e7a2118c4",
+    "url": "./static/js/2.7ba70dc8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7ba70dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aecf9506830afa939e9",
+    "url": "./static/js/main.6dc6852f.chunk.js"
+  },
+  {
+    "revision": "3a99927d370933ee096c",
+    "url": "./static/js/runtime-main.fd4eb36d.js"
+  }
+]);
